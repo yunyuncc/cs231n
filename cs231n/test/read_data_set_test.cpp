154afb3df7ba559b549475c9ed29a131da367303
yunyuncc <wyy584322202@outlook.com>
2018-02-14 21:22:27 +0800

    test how SetUp and TearDown use

--- cs231n/test/read_data_set_test.cpp
+++ cs231n/test/read_data_set_test.cpp
@@ -1,8 +1,15 @@
 #include <gmock/gmock.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
+using namespace std;
 using namespace testing;
+class data_set : public testing::Test {
+public:
+  void SetUp() { cout << "set up" << endl; }
+  void TearDown() { cout << "tear down" << endl; }
+};
+// how to set up and tear down
+TEST(read_data_set, to_cv_mat) { cv::Mat mat; }
 
-//how to set up and tear down
-TEST(read_data_set, to_cv_mat){
-  cv::Mat mat;
-}
+TEST_F(data_set, test_set_up_01) { cout << "test_set_up_01" << endl; }
+TEST_F(data_set, test_set_up_02) { cout << "test_set_up_02" << endl; }
